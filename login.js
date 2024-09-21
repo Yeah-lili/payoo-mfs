5f9a38592e2dd6bb657c5aef00e43b2ad78ac2ce
Yeah_Lili <yeasinkhalili557@gmail.com>
2024-09-22 02:12:07 +0600

    Home initial started

--- login.js
+++ login.js
@@ -6,11 +6,22 @@ document.getElementById('button-login').addEventListener('click',function(event)
 
     //step-2: prevent default behavior (prevent reloading browser)
     event.preventDefault(); //prevents reloading the page
-    console.log('login button clicked');
+    // console.log('login button clicked');
 
     //step-3:get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    //console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    //bad way to validate
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong phone number or pin.');
+    }
+        
 
 
 })
